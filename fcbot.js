09dae4b0f717e15ce305384e02d1432e9e12c595
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-06-22 23:18:03 +0300

    Heroku

--- fcbot.js
+++ fcbot.js
@@ -37,7 +37,7 @@ app.get("/", function (req, res) {
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
-    if (req.query["hub.verify_token"] === (process.env.verifyToken || keys.verifyToken)) {
+    if (req.query["hub.verify_token"] === (process.env.verifyToken/* || keys.verifyToken*/)) {
         console.log("FoodCompositionBot: Webhook verified");
         res.status(200).send(req.query["hub.challenge"]);
     } else {
